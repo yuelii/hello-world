820f42eb065e621812f4be666d00e059a8d07c4d
ee l <eel@eedeMacBook-Pro.local>
2021-03-20 22:31:02 +0800

    vscode hello_world

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+print ('hellor world')
+import json
+
+def greet(greeting,name):
+    """[summary]
+
+    Args:
+        greeting ([type]): [description]
+        name ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    return f'{greeting} {name}'
+
+
+## 另外一个插件：pytest（用于测试，但是需要python版本为3.8）
+## 需要在setting设置enable pytest，再conda install pytest
+## python test explorer
+
+# 在终端activate 各种环境，deactivate关闭环境
